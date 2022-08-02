fcd968100881fc92ddac7d4dacdae9a1696221b9
Ibim12 <ibim.obomanu@gmail.com>
2022-08-02 10:21:46 +0100

    second commit

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-print("rock")
+print(4%6)
+#if "input1"%5=0 and input2%4=0 and A#
+#print("rock wins")#
+#rock=4#
+#paper=5#
+#scissors==6#
+
+#input("What will you play: ")# 
+#print( input)#
+
+#input("What will opponent play :")#
+#print(input)#
+
