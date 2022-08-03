b58631d1a29ceed8483bbe236f458dbcb07f2183
Ibim12 <ibim.obomanu@gmail.com>
2022-08-03 09:09:58 +0100

    fff

---
+++ jj.py
@@ -0,0 +1,18 @@
+str(rock)=4
+str(paper)=5
+str(scissors)=6
+
+operation1= input("What will you play:" )
+
+
+operation2= input("What will your opponent play:" )
+
+print(operation1,operation2)
+
+
+if operation1%5 == 0 and operation2%4 == 0  :
+     
+    print("rock wins")
+
+
+
