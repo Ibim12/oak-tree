d601dbb4390262cce73f353c9cc613902b4a58c5
Ibim12 <ibim.obomanu@gmail.com>
2022-08-03 12:39:43 +0100

    jjj[

--- jj.py
+++ jj.py
@@ -1,14 +1,15 @@
 int(rock)=1
 int(paper)=2
 int(scissors)=3
-
+lizard
+spock
 operation1= input("What will you play:" )
 
 
 operation2= input("What will your opponent play:" )
 
 print(operation1,operation2)
-
+(operation1%3) (operation2%3)
 
 if operation1%5 == 0 and operation2%4 == 0  :
      
